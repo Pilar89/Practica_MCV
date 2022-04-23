9c57611e1ca9c471460136d00998a0d5b8aff87e
pilar <ingeniera.coral@gmail.com>
2022-04-23 01:40:09 -0500

    cambios main -- se crean las clase Board y BoardView

--- main.js
+++ main.js
@@ -1,37 +1,67 @@
-(function () {
-  var self.Board = function (width,height) {
-      this.width = width;
-      this.height = height;
-      this.playing = false;
-      this.game_over = false;
-      this.bars = [];
-      this.ball = null;
-      
+class Board {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.playing = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
   }
 
-  self.Board.prototype ={
-      get elements(){
-          var elements = this.bars;
-          elements.push(ball);
-          return elements;
-
-
-      }
+  get elements() {
+    var elements = this.bars;
+    elements.push(ball);
+    return elements;
   }
-})();
+}
 
-window.addEventListener("load",main);
+// (function () {
+//   var self.Board = function (width,height) {
+//       this.width = width;
+//       this.height = height;
+//       this.playing = false;
+//       this.game_over = false;
+//       this.bars = [];
+//       this.ball = null;
 
-(function () {
-    self.BoardView = function(canvas, board) {
-        this.canvas = canvas;
-        this.canvas.width = board.width;
-        this.canvas.height = board.height;
-        this.board = board;
-        this.contexto = canvas.getContext("2d");
-        
-    }
-    
-})();
+//   }
 
-function main(params) {}
+//   self.Board.prototype ={
+//       get elements(){
+//           var elements = this.bars;
+//           elements.push(ball);
+//           return elements;
+
+//       }
+//   }
+// })();
+
+window.addEventListener("load", main);
+
+class BoardView {
+  constructor(canvas, board) {
+    this.canvas = canvas;
+    this.canvas.width = board.width;
+    this.canvas.height = board.height;
+    this.board = board;
+    this.contexto = canvas.getContext("2d");
+  }
+}
+
+// (function () {
+//     self.BoardView = function(canvas, board) {
+//         this.canvas = canvas;
+//         this.canvas.width = board.width;
+//         this.canvas.height = board.height;
+//         this.board = board;
+//         this.contexto = canvas.getContext("2d");
+
+//     }
+
+// })();
+
+function main(params) {
+  var board = new Board(800, 400);
+  var canvas = document.getElementById("canvas");
+  var board_view = new BoardView(canvas, board);
+}
